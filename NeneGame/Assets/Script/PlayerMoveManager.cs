33e44dba310e6cc037b340ab34e7f4a3aeef2160
nunato <kyrie601@yahoo.co.jp>
2017-10-08 02:29:50 +0900

    プレイヤーの移動を追加

---
+++ NeneGame/Assets/Script/PlayerMoveManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveManager : MonoBehaviour
+{
+	public float speed;
+
+	private Rigidbody rbyPlayer;
+
+	void Start()
+	{
+		rbyPlayer = GetComponent<Rigidbody>();
+	}
+
+	void Update () 
+	{
+		float moveX = Input.GetAxis( "Horizontal");
+		float moveZ = Input.GetAxis( "Vertical" );
+
+		rbyPlayer.AddForce( moveX * speed, 0, moveZ * speed );
+	}
+}
