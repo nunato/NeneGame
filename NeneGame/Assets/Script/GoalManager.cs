6fab489e9665c7d9e51c04f54c05dc79f6c769c5
nunato <kyrie601@yahoo.co.jp>
2017-10-09 02:00:30 +0900

    壁とゴール追加

---
+++ NeneGame/Assets/Script/GoalManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalManager : MonoBehaviour
+{
+	void OnTriggerEnter( Collider cllider )
+	{
+		if( cllider.CompareTag( "Player" ) ){
+			Destroy( gameObject );
+		}
+	}
+}
