da7d43ab354e3c3cdcf8f0b5060ad0c7825b2e06
nunato <kyrie601@yahoo.co.jp>
2017-10-17 22:56:37 +0900

    カメラコントローラ追加

---
+++ NeneGame/Assets/Script/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform PlayerTransform;
+
+	private Vector3 offset;
+
+	void Start()
+	{
+		offset = transform.position - PlayerTransform.position;
+	}
+
+	void Update()
+	{
+		transform.position = PlayerTransform.position + offset;
+	}
+}
