f0a84a45ae35f53dc9f8e1404899eed5f4522700
nunato <kyrie601@yahoo.co.jp>
2017-10-22 19:34:13 +0900

    シーン遷移の追加

---
+++ NeneGame/Assets/Script/SceneLoad.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+	public void OnClick()
+	{
+		SceneManager.LoadScene("NeneGameStage1");
+	}
+}
