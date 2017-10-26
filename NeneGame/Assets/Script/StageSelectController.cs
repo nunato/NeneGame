766ff65cdaadb1eff047e05eebde070124407b7d
nunato <kyrie601@yahoo.co.jp>
2017-10-26 22:56:31 +0900

    ステージ選択画面の追加とステージ選択の修正

---
+++ NeneGame/Assets/Script/StageSelectController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelectController : MonoBehaviour
+{
+	public void OnClick( string sceneName )
+	{
+		if( sceneName != null ){
+			SceneManager.LoadScene(sceneName);
+		}
+	}
+}
