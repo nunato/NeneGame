975f82cd4cfd17468a8a41d0ce74f0b8e92216bb
nunato <kyrie601@yahoo.co.jp>
2017-10-29 23:14:40 +0900

    エリアアウト実装

---
+++ NeneGame/Assets/Script/ParticleDead.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDead : MonoBehaviour 
+{
+	private float lifeTime = 1f;
+
+	void Start () {
+		Destroy( gameObject, lifeTime );
+	}
+	
+}
---
+++ NeneGame/Assets/Script/PlayerDead.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDead : MonoBehaviour
+{
+	public int deadArea = -2;
+	public GameObject playerDeadParticle;
+
+	private Vector3 spownPoint;
+
+	void Start()
+	{
+		spownPoint = transform.position;
+	}
+
+	void Update()
+	{
+		if( transform.position.y < deadArea ){
+			Instantiate( playerDeadParticle, transform.position, Quaternion.identity );
+			transform.position = spownPoint;
+		}
+	}
+}
