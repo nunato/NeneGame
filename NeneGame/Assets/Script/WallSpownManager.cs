567d078039783d9e44c0a4d8876fb63d12971723
nunato <kyrie601@yahoo.co.jp>
2017-11-15 21:36:02 +0900

    壁の生成を追加

---
+++ NeneGame/Assets/Script/WallSpownManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpownManager : MonoBehaviour
+{
+	public GameObject wallObject;
+	public Transform spown;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Instantiate( wallObject, spown.position, Quaternion.identity );
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
