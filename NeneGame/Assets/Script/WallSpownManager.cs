894d1411a4eee5d3451710ee4e00d83364cf9307
nunato <kyrie601@yahoo.co.jp>
2017-11-16 23:43:10 +0900

    壁を連続して生成するように修正

--- NeneGame/Assets/Script/WallSpownManager.cs
+++ NeneGame/Assets/Script/WallSpownManager.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class WallSpownManager : MonoBehaviour
 {
 	public GameObject wallObject;
-	public Transform spown;
+	public Transform[] spownPoints;
+	public float spownSpeed_sec = 1.0f;
 
 	void Start ()
 	{
-		Instantiate( wallObject, spown.position, Quaternion.identity );
+		StartCoroutine( "WallSpown" );
+	}
+
+	private IEnumerator WallSpown()
+	{
+		while( true ){
+			int i = Random.Range(0, spownPoints.Length );
+			Instantiate( wallObject, spownPoints[i].position, Quaternion.identity );
+			yield return new WaitForSeconds( spownSpeed_sec );
+		}
 	}
 }
