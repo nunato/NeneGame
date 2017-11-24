a4b43dee052ead66466d19092e6a8c44885f8be6
nunato <kyrie601@yahoo.co.jp>
2017-11-25 03:18:21 +0900

    ハイスコアを保存するように追加

--- NeneGame/Assets/Script/ScoreManager.cs
+++ NeneGame/Assets/Script/ScoreManager.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
 	private static int GameScore = 0;
+	private static int HiScore = 0;
 	private static RespownManager respownManager;
+	private const string HIGH_SCORE_KEY = "HiScoreKey";
 
 	public static int gameScore
 	{
 		get { return GameScore; }
+	}
+
+	public static int hiScore
+	{
+		get{ return HiScore; }
 	}
 
 	void Start ()
@@ -17,6 +24,15 @@ public class ScoreManager : MonoBehaviour
 		GameScore = 0;
 		GameObject reManager = GameObject.Find("RespownManager");
 		respownManager = reManager.gameObject.GetComponent<RespownManager>();
+		HiScore = PlayerPrefs.GetInt(HIGH_SCORE_KEY, 0);
+	}
+
+	void Update()
+	{
+		if( GameScore > HiScore ){
+			HiScore = GameScore;
+			PlayerPrefs.SetInt( HIGH_SCORE_KEY, HiScore );
+		}
 	}
 
 	public static void AddScore( int newScore )
--- NeneGame/Assets/Script/ScoreTextController.cs
+++ NeneGame/Assets/Script/ScoreTextController.cs
@@ -6,14 +6,17 @@ using UnityEngine.UI;
 public class ScoreTextController : MonoBehaviour
 {
 	public Text ScoreText;
+	public Text HiScoreText;
 
 	void Start ()
 	{
 		ScoreText.text = "Score: " + ScoreManager.gameScore;
+		HiScoreText.text = "HiScore: " + ScoreManager.hiScore;
 	}
 
 	void Update ()
 	{
 		ScoreText.text = "Score: " + ScoreManager.gameScore;
+		HiScoreText.text = "HiScore: " + ScoreManager.hiScore;
 	}
 }
